1aefa766452b13cfc3cb910a784f3c39d934e156
Anthony <orlovskikh@gmail.com>
2020-11-26 18:43:45 +0300

    HW1 is completed and added comments

--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -9,13 +9,18 @@ import (
 )
 
 func main() {
+	// Узнаем текущее время на хосте
 	ct := time.Now()
+
+	// Узнаем точное время с использованием библиотеки ntp
 	et, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
 
+	// Обрабатываем ошибку обращения к удаленному серверу
 	if err != nil {
 		log.Fatal("Something wrong with ntp: ", err)
 	}
 
+	// Выводим результат с округлением времени, потраченного на вычисление (монотонное время)
 	fmt.Println("current time:", ct.Round(time.Nanosecond))
 	fmt.Println("exact time:", et.Round(time.Nanosecond))
 }
