c29999e025d73e14d4cee5d70aeeb988066b2173
Anthony <orlovskikh@gmail.com>
2020-12-09 10:35:26 +0300

    HW2 - change if-else logic, use Builder

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -11,38 +11,34 @@ var ErrInvalidString = errors.New("invalid string")
 
 // Функция распаковки строки по указанному количеству повторений символов.
 func Unpack(a string) (string, error) {
-	var result string = ""
-	var curLetter rune = 0
-	var resultErr error = nil
+	var (
+		curLetter rune // храним обрабатываемый символ для следующей итерации
+		result    strings.Builder
+	)
 
 	for _, v := range a {
-		//	fmt.Printf("%s = %d\n", string(v), v)
-
-		if unicode.IsDigit(v) && curLetter == 0 { //nolint
-			// Ошибка, если число вместо символа
-			result = ""
-			resultErr = ErrInvalidString
-			break
-		} else {
-			if unicode.IsDigit(v) {
-				// Повторяем символ по числу после него
-				j, _ := strconv.Atoi(string(v))
-				result += strings.Repeat(string(curLetter), j)
-				curLetter = 0
-			} else {
-				// Записываем одиночный символ
-				if curLetter != 0 {
-					result += string(curLetter)
-				}
-				curLetter = v // запоминаем символ на случай цифры после него
-			}
+		if unicode.IsDigit(v) && curLetter == 0 {
+			// Возвращаем ошибку, если число вместо символа
+			return "", ErrInvalidString
 		}
+		if unicode.IsDigit(v) {
+			// Повторяем символ по числу после него
+			j, _ := strconv.Atoi(string(v))
+			result.WriteString(strings.Repeat(string(curLetter), j))
+			curLetter = 0
+			continue
+		}
+		// Записываем одиночный символ
+		if curLetter != 0 {
+			result.WriteRune(curLetter)
+		}
+		curLetter = v // запоминаем символ на случай цифры после него
 	}
 
 	// Записываем последний символ, исключая нулевое значение руны
 	if !unicode.IsDigit(curLetter) && curLetter != 0 {
-		result += string(curLetter)
+		result.WriteRune(curLetter)
 	}
 
-	return result, resultErr
+	return result.String(), nil
 }
