ecc8498902028e55231c22c5d07e3b3b7be4d4e5
Anthony <orlovskikh@gmail.com>
2020-12-20 22:19:50 +0300

    HW3 - comlited. Checked lints and added comments.

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -5,61 +5,67 @@ import (
 	"unicode"
 )
 
+// Функция разделения текста на слова по пробелам.
 func unpack(a string) []string {
 	var (
-		curWord   string
 		result    []string
+		curWord   string
 		findSpace bool
 	)
 
 	for _, v := range a {
-
+		// Прупскаем любые пробелы, табуляции, перводы строки
 		if unicode.IsSpace(v) {
 			findSpace = true
 			continue
 		}
 
+		// По найденному пропуску после окончания слова записываем это слово в result
+		// Доп проверка на пробел в начале текста
 		if findSpace && curWord != "" {
 			result = append(result, curWord)
 			curWord = ""
 			findSpace = false
 		}
 
+		// Составляем слово по символьно
 		curWord += string(v)
 	}
+
+	// Записываем последнее слово в result
 	if curWord != "" {
 		result = append(result, curWord)
 	}
-
 	return result
 }
 
+// Функция поиска наиболее повторяющихся слов.
 func Top10(text string) []string {
-	s := unpack(text)
-	m := make(map[string]int)
-	//	fmt.Printf("%q\n", s)
-	for _, v := range s {
-		m[v] += 1
-	}
-	//	fmt.Println(m)
+	sepText := unpack(text)
 
-	var r []struct {
+	// Записываем все слова без повторений и считаем количество
+	countWords := make(map[string]int)
+	for _, v := range sepText {
+		countWords[v]++
+	}
+
+	r := []struct {
 		w string
 		n int
-	}
-	for k, v := range m {
-		a := struct {
+	}{}
+
+	// Переносим слова и их количество в структуру
+	for k, v := range countWords {
+		r = append(r, struct {
 			w string
 			n int
-		}{k, v}
-		r = append(r, a)
+		}{k, v})
 	}
-	//	fmt.Println(r)
 
+	// Сортируем слова по их количеству
 	sort.Slice(r, func(i, j int) bool { return r[i].n > r[j].n })
-	//	fmt.Println(r)
 
-	//	fmt.Println(strings.Count(text, "он"))
+	// Задаем максимальное число слов для возврата
 	h := 0
 	if len(r) < 10 {
 		h = len(r)
@@ -67,6 +73,7 @@ func Top10(text string) []string {
 		h = 10
 	}
 
+	// Заносим нужное количество слов для возрата из функции
 	var res []string
 	for i := 0; i < h; i++ {
 		res = append(res, r[i].w)
